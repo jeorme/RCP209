d28b707d32bb36ca3d43386089bce0983501b5ba
jerpetit <Fxinvjp79>
2019-01-10 14:49:26 +0100

    mlp in keras

--- TD10.py
+++ TD10.py
@@ -30,7 +30,26 @@ X_train, y_train, X_test, y_test = get_minst()
 Y_train, Y_test = convertToCat(y_train,y_test,nbCat=10)
 
 model.fit(X_train, Y_train,batch_size=batch_size, epochs=nb_epoch,verbose=1, callbacks=[tensorboard])
-saveModel(model,"mlp")
+saveModel(model,"mlp_sigmoid")
 scores = model.evaluate(X_test, Y_test, verbose=0)
+print("%s: %.2f%%" % (model.metrics_names[0], scores[0]*100))
+print("%s: %.2f%%" % (model.metrics_names[1], scores[1]*100))
+
+
+model_relu = Sequential()
+# construction d'un mlp multi couche
+# 1 hidden couche : activation sigmoid
+model_relu.add(Dense(100, input_dim=784, name='fc1'))
+model_relu.add(Activation('relu'))
+#couche de sortie prend automatique 100 comme input (couche précédente)
+model_relu.add(Dense(10, name='fc2'))
+model_relu.add(Activation('softmax'))
+print(model_relu.summary())
+sgd = SGD(learning_rate)
+model_relu.compile(loss='categorical_crossentropy', optimizer=sgd, metrics=['accuracy'])
+model_relu.fit(X_train, Y_train,batch_size=batch_size, epochs=nb_epoch,verbose=1)
+
+saveModel(model,"mlp_relu")
+scores = model_relu.evaluate(X_test, Y_test, verbose=0)
 print("%s: %.2f%%" % (model.metrics_names[0], scores[0]*100))
 print("%s: %.2f%%" % (model.metrics_names[1], scores[1]*100))
